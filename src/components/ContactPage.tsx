57f15d89ed5ed6d951ade86af5d0fc16cd061702
wr1159 <56021409+wr1159@users.noreply.github.com>
2024-07-01 00:03:43 +0800

    Rename ContactPage function to ContactPage

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -1,4 +1,4 @@
-export default function AboutPage() {
+export default function ContactPage() {
   return (
     <div className="flex flex-col">
       <section className="w-full py-12 md:py-24 lg:py-32">
