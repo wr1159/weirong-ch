86f8d1ba3dc6aaa215e738b9dbb6fe1ffcdbafa6
wr1159 <56021409+wr1159@users.noreply.github.com>
2024-06-30 19:13:56 +0800

    Add AboutPage

---
+++ src/components/AboutPage.tsx
@@ -0,0 +1,131 @@
+export default function AboutPage() {
+  return (
+    <div className="flex flex-col">
+      <section className="w-full py-12 md:py-24 lg:py-32">
+        <div className="px-4 md:px-6 grid gap-10 lg:grid-cols-5 lg:gap-12">
+          <div className="flex flex-col items-center justify-center space-y-4 text-center lg:col-span-2">
+            <div className="space-y-2">
+              <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+                Chu Wei Rong
+              </h1>
+              <p className="max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Hello, I am{" "}
+                <span className="font-bold text-accent-foreground">
+                  Wei Rong
+                </span>
+                , a penultimate Computer Science student at NUS.
+                <br />
+                <br />I love Software Engineering and other forms of engineering
+                as it allows me to create solutions to problems people face. I
+                particularly enjoy the blockchain space because of the constant
+                innovation to capture the attention economy.
+              </p>
+            </div>
+          </div>
+          <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-2 lg:col-span-3">
+            <div className="grid gap-1 content-start">
+              <h3 className="text-xl font-bold">Experience</h3>
+              <ul className="space-y-1 text-muted-foreground">
+                <li>
+                  Full Stack Intern at{" "}
+                  <a href="https://www.switcheo.com/">Switcheo Labs</a>
+                </li>
+                <li>
+                  Freelancer at{" "}
+                  <a href="https://www.solutions16.io/">Solutions16</a>
+                </li>
+                <li>
+                  SWE Intern, Freelancer at{" "}
+                  <a href="https://www.coingecko.com/">Coin</a>
+                  <a href="https://www.geckoterminal.com/">Gecko</a>
+                </li>
+                <li>
+                  Blockchain Director at{" "}
+                  <a href="https://fintechsociety.comp.nus.edu.sg/">NUS FTS</a>
+                </li>
+                <li>
+                  SWE Member at <a href="https://rhapp.lol">RHDevelopers</a>
+                </li>
+                <li>
+                  Teaching Assistant at{" "}
+                  <a href="https://cde.nus.edu.sg/edic/idp/">NUS EDIC</a>
+                </li>
+              </ul>
+            </div>
+            <div className="grid gap-1 content-start">
+              <h3 className="text-xl font-bold">Projects / Contributions</h3>
+              <ul className="space-y-1 text-muted-foreground">
+                <li>
+                  Contributor to{" "}
+                  <a href="https://github.com/cosmos/cosmos-sdk">Cosmos</a>
+                  <a href="https://github.com/cosmos/cosmos-sdk/pull/20517">
+                    SDK
+                  </a>{" "}
+                  and <a href="https://nusmods.com/planner">NUS</a>
+                  <a href="https://github.com/nusmodifications/nusmods/pulls?q=is:pr+author:wr1159+">
+                    Mods
+                  </a>
+                </li>
+                <li>
+                  <a href="https://github.com/marcuspang/only-frames">
+                    Content Monetisation
+                  </a>{" "}
+                  on Warpcast
+                </li>
+                <li>
+                  <a href="https://otamatone.vercel.app/">Otamatone PWA</a> to
+                  play the otamatone
+                </li>
+                <li>
+                  <a href="https://github.com/wr1159/r2auto_nav">
+                    Can Delivery Robot
+                  </a>{" "}
+                  and{" "}
+                  <a href="https://github.com/wr1159/r2auto_nav/tree/main/ESP32">
+                    Dispenser
+                  </a>
+                </li>
+                <li>
+                  <a href="https://devpost.com/software/knock-it-down">
+                    Pushup Counting ML Model
+                  </a>
+                </li>
+                <li>and more...</li>
+              </ul>
+            </div>
+            <div className="grid gap-1 content-start">
+              <h3 className="text-xl font-bold">Languages</h3>
+              <ul className="text-muted-foreground grid-cols-2 grid gap-y-1">
+                <li>Go</li>
+                <li>Java</li>
+                <li>JavaScript</li>
+                <li>Protobuf</li>
+                <li>Python</li>
+                <li>Solidity</li>
+                <li>SQL</li>
+                <li>TypeScript</li>
+              </ul>
+            </div>
+            <div className="grid gap-1 content-start">
+              <h3 className="text-xl font-bold">Frameworks / Tools</h3>
+              <ul className="text-muted-foreground grid-cols-2 grid gap-y-1">
+                <li>Astro</li>
+                <li>CosmosSDK</li>
+                <li>Docker</li>
+                <li>Django</li>
+                <li>Git</li>
+                <li>HardHat</li>
+                <li>Kubernetes</li>
+                <li>NextJS</li>
+                <li>PostgreSQL</li>
+                <li>Prisma</li>
+                <li>React</li>
+                <li>TailwindCSS</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,9 @@
 import { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetTitle, SheetDescription, SheetFooter, SheetClose } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "./ModeToggle";
+import { MenuIcon, MountainIcon } from "./Icons";
 
-export function Navbar() {
+export default function Navbar() {
   return (
     <header className="flex h-20 w-full shrink-0 items-center">
       <Sheet>
@@ -51,64 +52,24 @@ export function Navbar() {
       <nav className="ml-auto hidden sm:flex gap-4">
         <a
           href="#"
-          className="group inline-flex h-9 w-max items-center justify-center rounded-md px-2 py-2 text-sm font-medium"
+          className="group inline-flex h-9 w-max items-center justify-center rounded-md px-2 py-2 text-sm font-medium hover:text-muted-foreground"
         >
           About
         </a>
         <a
           href="#"
-          className="group inline-flex h-9 w-max items-center justify-center rounded-md px-2 py-2 text-sm font-medium" 
+          className="group inline-flex h-9 w-max items-center justify-center rounded-md px-2 py-2 text-sm font-medium hover:text-muted-foreground" 
         >
           Resume
         </a>
         <a
           href="#"
-          className="group inline-flex h-9 w-max items-center justify-center rounded-md px-2 py-2 text-sm font-medium"
+          className="group inline-flex h-9 w-max items-center justify-center rounded-md px-2 py-2 text-sm font-medium hover:text-muted-foreground"
         >
           Contact
         </a>
         <ModeToggle />
       </nav>
     </header>
-  );
-}
-
-function MenuIcon({ className }: { className: string }) {
-  return (
-    <svg
-      className={className}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <line x1="4" x2="20" y1="12" y2="12" />
-      <line x1="4" x2="20" y1="6" y2="6" />
-      <line x1="4" x2="20" y1="18" y2="18" />
-    </svg>
-  );
-}
-
-function MountainIcon({ className }: { className: string }) {
-  return (
-    <svg
-      className={className}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-    </svg>
   );
 }
