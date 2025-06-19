c2cc2863488cd3068141f3236b4bae7f8064de71
wr1159 <chuweiron18@gmail.com>
2025-06-19 06:14:43 +0100

    Hackathons: Create basic hackathons page

---
+++ src/components/HackathonsPage.tsx
@@ -0,0 +1,135 @@
+export default function HackathonsPage() {
+  return (
+    <div className="flex flex-col">
+      <section className="w-full py-12 md:py-24 lg:py-32">
+        <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl pb-2">
+          Hackathons I've Won (Most Recent First)
+        </h1>
+        <ol className="space-y-1 text-muted-foreground py-4 gap-y-1">
+          <li>
+            <a
+              href="https://dorahacks.io/buidl/26511"
+              rel="nofollow noindex noreferrer"
+            >
+              ETHDublin 2025
+            </a>
+            &nbsp;- Base 1st Place & Finalist - Social Habit-breaking Platform
+          </li>
+          <li>
+            <a
+              href="https://x.com/wr1159/status/1923394297816051821"
+              rel="nofollow noindex noreferrer"
+            >
+              Consensus 2025 EasyA Hackathon
+            </a>
+            &nbsp;- Polkadot 1st Place & Forte 3rd Place - NFC-based
+            Proof-Of-Location System
+          </li>
+          <li>
+            <a
+              href="https://x.com/wr1159/status/1912215613973409836"
+              rel="nofollow noindex noreferrer"
+            >
+              Encode AI Hackathon 2025
+            </a>
+            &nbsp;- Zora 2nd Place - Brainrot Zora Coin Generator
+          </li>
+          <li>
+            <a
+              href="https://dorahacks.io/buidl/25723"
+              rel="nofollow noindex noreferrer"
+            >
+              UK AI Hackathon 2025
+            </a>
+            &nbsp;- Venice 1st Place & FLockIo 2nd Place & EduChain 3rd Place -
+            AI Gamified EduFi Platform
+          </li>
+          <li>
+            <a
+              href="https://dorahacks.io/buidl/25565"
+              rel="nofollow noindex noreferrer"
+            >
+              ETHBucharest 2025
+            </a>
+            &nbsp;- Base Bounty Winner - Milestone-based Crowdfunding Platform
+          </li>
+          <li>
+            <a
+              href="https://github.com/wr1159/BrisHack-2025"
+              rel="nofollow noindex noreferrer"
+            >
+              BrisHack 2025
+            </a>
+            &nbsp;- Most Technical Hack - Crowdsourcing Data Collection
+          </li>
+          <li>
+            <a
+              href="https://dorahacks.io/buidl/22584"
+              rel="nofollow noindex noreferrer"
+            >
+              ETHOxford 2025
+            </a>
+            &nbsp;- Honourable Mention & AirDAO 1st Place - On-Chain AI
+            Deduction Game
+          </li>
+          <li>
+            <a
+              href="https://dorahacks.io/buidl/20738"
+              rel="nofollow noindex noreferrer"
+            >
+              Taipei Blockchain Week 2024
+            </a>
+            &nbsp;- Soon Bounty Winner - Token Basket Creation and Tracking
+          </li>
+          <li>
+            <a
+              href="https://cde.nus.edu.sg/edic/projects/innovations-in-intelligent-systems/sam/"
+              rel="nofollow noindex noreferrer"
+            >
+              SLA GNSS Innovation Quest
+            </a>
+            &nbsp;- 1st Place - Autonomous Invasive Creeper Detecting Robot
+          </li>
+          <li>
+            <a
+              href="https://ethglobal.com/showcase/co-in-munity-d9m3d"
+              rel="nofollow noindex noreferrer"
+            >
+              ETHGlobal Singapore 2024
+            </a>
+            &nbsp;- 1inch 2nd Place & AirDAO 1st Place - Community Token
+            Launchpad
+          </li>
+          <li>
+            <a
+              href="https://ethglobal.com/showcase/ordisynth-8o26d"
+              rel="nofollow noindex noreferrer"
+            >
+              ETHOnline 2024
+            </a>
+            &nbsp;- Best Runes or Ordinal project on Rootstoc - Ordinal
+            Synthetic Market
+          </li>
+          <li>
+            <a
+              href="https://github.com/marcuspang/only-frames"
+              rel="nofollow noindex noreferrer"
+            >
+              Singapore Base Frame Hackathon 2024
+            </a>
+            &nbsp;- Most Creative Hack - Content Monetisation on Farcaster
+          </li>
+          <li>
+            <a
+              href="https://devpost.com/software/knock-it-down"
+              rel="nofollow noindex noreferrer"
+            >
+              NUS Hacknroll 2023
+            </a>
+            &nbsp;- Best Freshman Hacks - Push Up Counter
+          </li>
+        </ol>
+      </section>
+    </div>
+  );
+}
